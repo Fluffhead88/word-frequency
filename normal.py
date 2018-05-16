10c37ecddb8039605b70aaec101b1c4ddef8482c
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-16 16:14:47 -0400

    Playing with sort function

--- normal.py
+++ normal.py
@@ -1,20 +1,21 @@
 # normal version of word frequency program
 
+import operator
 my_dict = {}
 
 with open('sample.txt') as infile:
     my_contents = infile.read()
-    my_words = my_contents.lower().replace("!", "").replace(":", "").replace(".", "").replace("\n", "").replace("#", "").replace("?", "").replace("-", "").replace(",", "").split()
+    my_words = my_contents.lower().replace("!", "").replace(":", "").replace(".", "").replace("\n", " ").replace("?", "").replace("-", " ").replace(",", " ").split()
 
     for word in my_words:
 
         if word not in my_dict:
             my_dict[word] = 0
         my_dict[word] += 1
-        if my_dict[word] > 1500:
-            print(my_dict[word], word)
 
 print(my_dict)
-for occur in my_dict:
-    if occur > 1500:
-        print (occur)
+
+
+#def sort_words(x):
+#    return x[1]
+#sorted(my_dict, key=sort_words)
