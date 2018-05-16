21ac5658b65a8a49551dbfd280fb967f08ab15ca
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-16 13:16:31 -0400

    Adding text file and normal file with contents normalized, all lower case, no special characters

--- normal.py
+++ normal.py
@@ -1,1 +1,9 @@
 # normal version of word frequency program
+
+with open('sample.txt') as infile:
+    my_contents = infile.read()
+    my_contents.replace("!", "").replace(",", "").replace(":", "").replace(".", "").replace("/n", "").replace("*", "").replace("#", "").replace("?", "")
+    my_contents.lower()
+    my_content.replace("(", "").replace(")", "")
+    my_contents.replace(("-", "").replace("[", "").replace("]", "").replace(""", "")
+    
