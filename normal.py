79afa0fe54ed3a53c8c6a58d9f6f8e3841cf8eea
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-16 13:36:16 -0400

    First draft

--- normal.py
+++ normal.py
@@ -2,8 +2,16 @@
 
 with open('sample.txt') as infile:
     my_contents = infile.read()
-    my_contents.replace("!", "").replace(",", "").replace(":", "").replace(".", "").replace("/n", "").replace("*", "").replace("#", "").replace("?", "")
+    my_contents.replace("!", "").replace(":", "").replace(".", "").replace("/n", "").replace("*", "").replace("#", "").replace("?", "").replace(("-", "").replace(",", "")
     my_contents.lower()
-    my_content.replace("(", "").replace(")", "")
-    my_contents.replace(("-", "").replace("[", "").replace("]", "").replace(""", "")
-    
+
+    my_words = my_contents.split()
+
+        my_dict = {}
+
+    for word in my_words:
+        if word not in my_dict.keys():
+            my_dict[word] = 0
+            my_dict[word] += 1
+        if my_dict[word] > 40
+        print my_dict[word]
